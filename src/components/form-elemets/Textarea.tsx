d0f89abbd9b463cfeba10ce040f68bd96582f698
snehamoybag <102692389+snehamoybag@users.noreply.github.com>
2025-09-28 13:55:15 +0530

    refractor: make Textarea element auto resize on change

--- src/components/form-elemets/Textarea.tsx
+++ src/components/form-elemets/Textarea.tsx
@@ -1,18 +1,45 @@
-import type { ReactElement, TextareaHTMLAttributes } from "react";
+import {
+  type ChangeEventHandler,
+  type ReactElement,
+  type TextareaHTMLAttributes,
+} from "react";
 
 interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   value?: string;
+  onChange?: ChangeEventHandler<HTMLTextAreaElement>;
   className?: string;
 }
 
 export default function Textarea({
   className = "",
   value = "",
+  onChange,
   ...restProps
 }: Readonly<TextareaProps>): ReactElement {
+  const handleChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+    if (!onChange) {
+      return;
+    }
+
+    const elem = e.target;
+
+    // auto resize on type
+    const scrollHeight = elem.scrollHeight;
+
+    elem.style.height = "auto"; // reset size first
+
+    if (scrollHeight) {
+      elem.style.height = scrollHeight + "px";
+    }
+
+    // call the onChange event
+    onChange(e);
+  };
+
   return (
     <textarea
-      className={`p-4 border-1 border-solid border-neutral-700 rounded-xl ${className}`}
+      className={`min-h-[5lh] p-4 border-1 border-solid border-neutral-700 rounded-xl resize-none overflow-hidden ${className}`}
+      onChange={handleChange}
       {...restProps}
     ></textarea>
   );
